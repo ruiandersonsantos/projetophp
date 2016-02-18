0d4e0bd1e23f7c48194f4ed8e572bb57ae6dc2e8
ruiandersonsantos <ruiandersonsantos@gmail.com>
2016-02-18 00:39:09 -0200

    Alteração da conexão com o BD - Rui

--- application/config/database.php
+++ application/config/database.php
@@ -47,18 +47,22 @@
 
 $active_group = 'default';
 $active_record = TRUE;
-$db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'simone';
-$db['default']['password'] = '442332si';
-$db['default']['database'] = 'projetophp';
+
 //$db['default']['hostname'] = 'localhost';
-//$db['default']['username'] = 'root';
-//$db['default']['password'] = '42301886';
-//$db['default']['database'] = 'projetphp';
+//$db['default']['username'] = 'simone';
+//$db['default']['password'] = '442332si';
+//$db['default']['database'] = 'projetophp';
+
+$db['default']['hostname'] = 'localhost';
+$db['default']['username'] = 'root';
+$db['default']['password'] = '42301886';
+$db['default']['database'] = 'projetpphp';
+
 //$db['default']['hostname'] = 'ruianderson.com.br';
 //$db['default']['username'] = 'ruiand_root';
 //$db['default']['password'] = '42301886';
 //$db['default']['database'] = 'ruiand_projetophp';
+
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
