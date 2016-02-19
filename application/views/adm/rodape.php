fb00bb03429e358b0578b5d0c2fdcc2de15a8d8b
Simone <Simone@Simone-PC>
2016-02-19 19:07:47 -0200

    Ajuste do rodape 19/02/2015 - simone

--- application/views/adm/rodape.php
+++ application/views/adm/rodape.php
@@ -1,10 +1,4 @@
-   <!-- rodape -->
-        <footer>
-            <div class="footer" style="position: relative;">
-                Todos os Direitos Reservados a <strong>RS Soluções</strong>
-            </div>
-        </footer>
-        <!-- ./rodape -->
+   
 
         <script src="<?= base_url("js_b/jquery-1.11.3.min.js")?>"></script>
         <script src="<?= base_url("js_b/bootstrap.min.js")?>"></script>
